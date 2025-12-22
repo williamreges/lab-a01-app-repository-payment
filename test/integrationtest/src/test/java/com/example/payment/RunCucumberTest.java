65ec40e91e3a5d271bb6d49367fb7cbd3952664c
william-reges <william.reges1986@gmail.com>
2025-12-22 19:47:04 -0300

    test: criando teste com cucumber no repositório

---
+++ test/integrationtest/src/test/java/com/example/payment/RunCucumberTest.java
@@ -0,0 +1,21 @@
+package com.example.payment;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.ConfigurationParametersResource;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, " +
+        "html:target/cucumber-reports/cucumber-report.html, " +
+        "json:target/cucumber-reports/cucumber.json")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.payment.steps")
+@ConfigurationParametersResource("cucumber.properties")
+public class RunCucumberTest {
+}
---
+++ test/integrationtest/src/test/resources/features/TransacaoPixGet.feature
@@ -0,0 +1,8 @@
+Feature: : Lista de PIX
+
+  Scenario: Obter uma lista de PIX
+    Given quero poder passar um Id PIX
+    Then quero obter o resultado
+
+
+
